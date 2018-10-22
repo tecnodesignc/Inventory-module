b6757fb584cc4b2580731d85a7e7c7b97c05f757
Marcos RAtiva <marcos21.009@gmail.com>
2018-10-21 22:58:49 -0500

    change Translate Transactions

---
+++ Repositories/Eloquent/EloquentTransactionRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Inventory\Repositories\Eloquent;
+
+use Modules\Inventory\Repositories\TransactionRepository;
+use Modules\Core\Repositories\Eloquent\EloquentBaseRepository;
+
+class EloquentTransactionRepository extends EloquentBaseRepository implements TransactionRepository
+{
+}
